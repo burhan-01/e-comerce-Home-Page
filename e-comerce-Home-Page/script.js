6c71783934bbd8b21eb41cef8625ce6f7a0ef801
burhan-01 <mburhanqa@gmail.com>
2024-08-12 14:23:51 +0500

    Add files via upload

---
+++ e-comerce-Home-Page/script.js
@@ -0,0 +1,22 @@
+// On-Scroll Animations
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+  threshold: 0.1,
+  rootMargin: '0px 0px -50px 0px'
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('show');
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
